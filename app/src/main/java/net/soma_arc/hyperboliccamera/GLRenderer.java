97d670ddee610c5ba18156c48e61cfd5f3c3a3d8
soma_arc <somaarcr@gmail.com>
2016-11-10 17:14:45 +0900

    Save image

--- app/src/main/java/net/soma_arc/hyperboliccamera/GLRenderer.java
+++ app/src/main/java/net/soma_arc/hyperboliccamera/GLRenderer.java
@@ -1,9 +1,26 @@
 package net.soma_arc.hyperboliccamera;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.os.Environment;
 import android.os.SystemClock;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.IntBuffer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -61,4 +78,66 @@ public class GLRenderer implements GLSurfaceView.Renderer {
         rect.draw(resolution, 0);
     }
 
+    public static Bitmap capture(final int w, final int h){
+        final int pixels[] = new int[w * h];
+        final int result[] = new int[w * h];
+        IntBuffer buffer = IntBuffer.wrap(pixels);
+        buffer.position(0);
+        GLES20.glReadPixels(0, 0, w, h, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, buffer);
+
+        // This code is based on http://www.anddev.org/how_to_get_opengl_screenshot__useful_programing_hint-t829.html
+        // See also http://d.hatena.ne.jp/orangesignal/20120814/1344923993
+        for(int i=0; i<h; i++) {
+            for(int j=0; j<w; j++) {
+                int pix=pixels[i*w+j];
+                int pb=(pix>>16)&0xff;
+                int pr=(pix<<16)&0x00ff0000;
+                int pix1=(pix&0xff00ff00) | pr | pb;
+                result[(h-i-1)*w+j]=pix1;
+            }
+        }
+        return Bitmap.createBitmap(result, w, h, Bitmap.Config.ARGB_8888);
+    }
+
+    public void savePNG()
+    {
+        if (ContextCompat.checkSelfPermission(
+                activity, Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED) {
+            Bitmap bmp = capture((int) resolution[0], (int) resolution[1]);
+
+            Date date = new Date();
+            SimpleDateFormat name = new SimpleDateFormat("yyyyMMdd_HHmmss");
+            final int quality = 100;
+            File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            try {
+                path.mkdir();
+                File file = new File(path, name.format(date) +".png");
+                FileOutputStream fos = new FileOutputStream(file);
+                bmp.compress(Bitmap.CompressFormat.PNG, quality, fos);
+                fos.flush();
+                fos.close();
+
+                // from  https://developer.android.com/reference/android/os/Environment.html
+                MediaScannerConnection.scanFile(activity,
+                        new String[] { file.toString() }, null,
+                        new MediaScannerConnection.OnScanCompletedListener() {
+                            public void onScanCompleted(String path, Uri uri) {
+                                Log.i("ExternalStorage", "Scanned " + path + ":");
+                                Log.i("ExternalStorage", "-> uri=" + uri);
+                            }
+                        });
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+            } else {
+                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+            }
+        }
+
+    }
 }
--- app/src/main/java/net/soma_arc/hyperboliccamera/MyGLSurfaceView.java
+++ app/src/main/java/net/soma_arc/hyperboliccamera/MyGLSurfaceView.java
@@ -3,6 +3,10 @@ package net.soma_arc.hyperboliccamera;
 import android.app.Activity;
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
 /**
  * Created by soma on 2016/11/07.
@@ -10,6 +14,7 @@ import android.opengl.GLSurfaceView;
 
 public class MyGLSurfaceView extends GLSurfaceView {
     private  final GLRenderer renderer;
+    private GestureDetector gestureDetector;
 
     public MyGLSurfaceView(Context context){
         super(context);
@@ -21,5 +26,28 @@ public class MyGLSurfaceView extends GLSurfaceView {
         setRenderer(renderer);
 
         //setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+
+        gestureDetector = new GestureDetector(context, onGestureListener);
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        gestureDetector.onTouchEvent(e);
+        return true;
+    }
+
+    private final GestureDetector.SimpleOnGestureListener onGestureListener = new GestureDetector.SimpleOnGestureListener(){
+        @Override
+        public boolean onDoubleTap(MotionEvent e){
+            Toast.makeText(getContext(), "Saving...", Toast.LENGTH_LONG).show();
+
+            queueEvent(new Runnable() {
+                @Override
+                public void run() {
+                    renderer.savePNG();
+                }
+            });
+            return super.onDoubleTap(e);
+        }
+    };
 }
